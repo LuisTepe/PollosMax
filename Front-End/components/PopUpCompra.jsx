d7652a1c34b99469938e43b4dd5c7ecff562cafc
Ismael de la Rosa <ismaeldlr10@gmail.com>
2024-04-10 18:40:34 -0700

    Task0010- Color Menu terminado

--- Front-End/components/PopUpCompra.jsx
+++ Front-End/components/PopUpCompra.jsx
@@ -7,16 +7,16 @@ import { handleKeyDownForNumericInputs, handleValueForNumericInputs } from "../s
 import axios from 'axios';
 import { getLoggedInUser } from '../src/pages/LoginPage';
 
-export default function PopUpCompra ({ onClose, total, onCompra}) {
+export default function PopUpCompra({ onClose, total, onCompra }) {
     const [color, setColor] = useState({
-        color: 'rgba(0, 0, 0, 1)',
-        borderColor: 'rgba(0, 0, 0, 1)',
-        borderTopColor: 'rgba(0, 0, 0, 1)',
-        borderRightColor: 'rgba(0, 0, 0, 1)',
-        borderBottomColor: 'rgba(0, 0, 0, 1)',
+        color: ' rgb(49, 48, 77)',
+        borderColor: ' rgb(49, 48, 77)',
+        borderTopColor: ' rgb(49, 48, 77)',
+        borderRightColor: ' rgb(49, 48, 77)',
+        borderBottomColor: ' rgb(49, 48, 77)',
     })
 
-    const [selectedButton, setSelectedButton] = useState(null);
+    const [selectedButton, setSelectedButton] = useState("Efectivo");
     const [quantity, setQuantity] = useState("");
     const [popupMessage, setPopupMessage] = useState('');
     const [popupImage, setPopupImage] = useState('');
@@ -33,8 +33,8 @@ export default function PopUpCompra ({ onClose, total, onCompra}) {
     const buttonStyle = (buttonName) => ({
         marginTop: '20px',
         fontFamily: 'Allerta',
-        background: selectedButton === buttonName ? 'purple' : '#fbd23a',
-        color: selectedButton === buttonName ? 'white' : 'black',
+        background: selectedButton === buttonName ? 'rgb(23, 22, 36)' : 'rgb(49, 48, 77)',
+        color: selectedButton === buttonName ? 'white' : 'white',
         borderWidth: '0px',
         borderColor: color.borderColor,
         borderTopColor: color.borderTopColor,
@@ -45,31 +45,31 @@ export default function PopUpCompra ({ onClose, total, onCompra}) {
 
     function handleMouseUp() {
         setColor({
-            color: 'rgba(0, 0, 0, 1)',
-            borderColor: 'rgba(0, 0, 0, 1)',
-            borderTopColor: 'rgba(0, 0, 0, 1)',
-            borderRightColor: 'rgba(0, 0, 0, 1)',
-            borderBottomColor: 'rgba(0, 0, 0, 1)',
+            color: ' rgb(49, 48, 77)',
+            borderColor: ' rgb(49, 48, 77)',
+            borderTopColor: ' rgb(49, 48, 77)',
+            borderRightColor: ' rgb(49, 48, 77)',
+            borderBottomColor: ' rgb(49, 48, 77)',
         })
     }
 
     function handleMouseDown() {
         setColor({
-            color: 'rgba(30, 30, 30, 1)',
-            borderColor: 'rgba(30, 30, 30, 1)',
-            borderTopColor: 'rgba(30, 30, 30, 1)',
-            borderRightColor: 'rgba(30, 30, 30, 1)',
-            borderBottomColor: 'rgba(30, 30, 30, 1)',
+            color: ' rgb(49, 48, 77)',
+            borderColor: ' rgb(49, 48, 77)',
+            borderTopColor: ' rgb(49, 48, 77)',
+            borderRightColor: ' rgb(49, 48, 77)',
+            borderBottomColor: ' rgb(49, 48, 77)',
         })
     }
 
     function handleHover() {
         setColor({
-            color: 'rgba(30, 30, 30, 1)',
-            borderColor: 'rgba(30, 30, 30, 1)',
-            borderTopColor: 'rgba(30, 30, 30, 1)',
-            borderRightColor: 'rgba(30, 30, 30, 1)',
-            borderBottomColor: 'rgba(30, 30, 30, 1)',
+            color: ' rgb(49, 48, 77)',
+            borderColor: ' rgb(49, 48, 77)',
+            borderTopColor: ' rgb(49, 48, 77)',
+            borderRightColor: ' rgb(49, 48, 77)',
+            borderBottomColor: ' rgb(49, 48, 77)',
         })
     }
 
@@ -87,7 +87,7 @@ export default function PopUpCompra ({ onClose, total, onCompra}) {
                 setPopupMessage('Pago exitoso, su cambio es de ' + change.toFixed(2) + ' pesos');
                 setShowPopup(true);
                 console.log(`El cambio es: ${change}`);
-                registrarVenta(2);   
+                registrarVenta(2);
                 // Aquí puedes hacer algo con el cambio, como mostrarlo en la interfaz de usuario
             } else {
                 setPopupImageFail("../src/assets/img/error.png");
@@ -102,7 +102,7 @@ export default function PopUpCompra ({ onClose, total, onCompra}) {
     async function registrarVenta(metodoPago) {
         var usuario;
         console.log(user);
-        if(user === 'admin') {
+        if (user === 'admin') {
             usuario = 1;
         } else if (user === 'user') {
             usuario = 2;
@@ -111,7 +111,7 @@ export default function PopUpCompra ({ onClose, total, onCompra}) {
             console.error('Error al registrar la venta:', 'No se pudo obtener el tipo de usuario');
             return;
         }
-    
+
         // Aquí puedes registrar la venta en la base de datos
         try {
             const response = await axios.post('http://localhost:3000/transaction', {
@@ -119,7 +119,7 @@ export default function PopUpCompra ({ onClose, total, onCompra}) {
                 idMovementType: metodoPago,
                 totalPrice: total,
             });
-    
+
             console.log('Venta registrada:', response.data);
         } catch (error) {
             console.error('Error al registrar la venta:', error);
@@ -129,49 +129,49 @@ export default function PopUpCompra ({ onClose, total, onCompra}) {
     const handleClosePopup = () => {
         setShowPopup(false);
         onCompra();
-      }
-    
+    }
+
     const handleClosePopupFail = () => {
         setShowPopupFail(false);
-        }
+    }
 
     return (
         <div>
-        <div className="modal-container" id="modal">
-            <div className="modal-content d-inline-flex justify-content-lg-center align-items-lg-center" style={{ background: 'rgb(251, 225, 147)', width: '400px', height: '500px', padding: '75px', borderRadius: '10px', border: '2px solid var(--bs-emphasis-color)', marginTop: '80px' }}>
-                <div style={{ width: '350px', textAlign: 'center', height: '500px' }}>
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <div>Total sin redondeo: ${total.toFixed(2)}</div>
-                        <div>Total redondeado: ${Math.ceil(total)}</div>
-                    </div>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
-                        <button onClick={() => handleButtonClick('Efectivo')} style={buttonStyle('Efectivo')} className="btn btn-primary">Efectivo</button>
-                        <button onClick={() => handleButtonClick('Tarjeta')} style={buttonStyle('Tarjeta')} className="btn btn-primary">Tarjeta</button>
-                        <button onClick={() => handleButtonClick('Transferencia')} style={buttonStyle('Transferencia')} className="btn btn-primary">Transferencia</button>
-                    </div>
+            <div className="modal-container" id="modal">
+                <div className="modal-content d-inline-flex justify-content-lg-center align-items-lg-center" style={{ background: 'rgb(182, 187, 196)', width: '400px', height: '500px', padding: '75px', borderRadius: '10px', border: '2px solid var(--bs-emphasis-color)', marginTop: '80px' }}>
+                    <div style={{ width: '350px', textAlign: 'center', height: '500px' }}>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <div>Total sin redondeo: ${total.toFixed(2)}</div>
+                            <div>Total redondeado: ${Math.ceil(total)}</div>
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
+                            <button onClick={() => handleButtonClick('Efectivo')} style={buttonStyle('Efectivo')} className="btn btn-primary">Efectivo</button>
+                            <button disabled onClick={() => handleButtonClick('Tarjeta')} style={buttonStyle('Tarjeta')} className="btn btn-primary">Tarjeta</button>
+                            <button disabled onClick={() => handleButtonClick('Transferencia')} style={buttonStyle('Transferencia')} className="btn btn-primary">Transferencia</button>
+                        </div>
 
-                    {selectedButton === 'Efectivo' && (
-                        <><div style={{ marginTop: '30px' }}>
-                            <p style={{ marginTop: '10px', fontFamily: 'Allerta', textAlign: 'left', fontSize: '18px' }}>Cantidad Recibida</p>
-                        </div><div>
-                                <div className="text-start" style={{ borderRadius: '5px', color: 'rgb(157,153,153)', background: '#D9D9D9', padding: '10px', border: '2px solid var(--bs-emphasis-color)', width: '330px' }}>
-                                    <input 
-                                    type="number" 
-                                    style={{ color: 'black', background: 'rgba(255,255,255,0)', borderColor: 'rgba(194,186,186,0)', outline: 'none' }} 
-                                    value = {quantity? quantity : ''}
-                                    onChange={(e) => handleValueForNumericInputs(e, setQuantity, quantity)} 
-                                    onKeyDown={handleKeyDownForNumericInputs}
-                                    />
-                                </div>
-                            </div></>
-                    )}
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <button onClick={onClose} className="btn btn-primary" type="button" style={{ marginTop: '20px', fontFamily: 'Allerta', background: color.color, borderWidth: '5px', borderColor: color.borderColor, borderTopColor: color.borderTopColor, borderRightColor: color.borderRightColor, borderBottomColor: color.borderBottomColor, outline: 'none' }}>Cancelar</button>
-                        <button onClick={handlePago} className="btn btn-primary" type="button" style={{ marginTop: '20px', fontFamily: 'Allerta', background: color.color, borderWidth: '5px', borderColor: color.borderColor, borderTopColor: color.borderTopColor, borderRightColor: color.borderRightColor, borderBottomColor: color.borderBottomColor, outline: 'none' }} onMouseUp={handleMouseUp} onMouseDown={handleMouseDown} onMouseOver={handleHover} onMouseLeave={handleMouseUp}>Pagar</button>
-                    </div>                    
+                        {selectedButton === 'Efectivo' && (
+                            <><div style={{ marginTop: '30px' }}>
+                                <p style={{ marginTop: '10px', fontFamily: 'Allerta', textAlign: 'left', fontSize: '18px' }}>Cantidad Recibida</p>
+                            </div><div>
+                                    <div className="text-start" style={{ borderRadius: '5px', color: 'rgb(157,153,153)', background: '#D9D9D9', padding: '10px', border: '2px solid var(--bs-emphasis-color)', width: '330px' }}>
+                                        <input
+                                            type="number"
+                                            style={{ color: 'black', background: 'rgba(255,255,255,0)', borderColor: 'rgba(194,186,186,0)', outline: 'none' }}
+                                            value={quantity ? quantity : ''}
+                                            onChange={(e) => handleValueForNumericInputs(e, setQuantity, quantity)}
+                                            onKeyDown={handleKeyDownForNumericInputs}
+                                        />
+                                    </div>
+                                </div></>
+                        )}
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <button onClick={onClose} className="btn btn-primary" type="button" style={{ marginTop: '20px', fontFamily: 'Allerta', background: color.color, borderWidth: '5px', borderColor: color.borderColor, borderTopColor: color.borderTopColor, borderRightColor: color.borderRightColor, borderBottomColor: color.borderBottomColor, outline: 'none' }}>Cancelar</button>
+                            <button onClick={handlePago} className="btn btn-primary" type="button" style={{ marginTop: '20px', fontFamily: 'Allerta', background: color.color, borderWidth: '5px', borderColor: color.borderColor, borderTopColor: color.borderTopColor, borderRightColor: color.borderRightColor, borderBottomColor: color.borderBottomColor, outline: 'none' }} onMouseUp={handleMouseUp} onMouseDown={handleMouseDown} onMouseOver={handleHover} onMouseLeave={handleMouseUp}>Pagar</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
             {showPopup && (<PopupPay image={popupImage} message={popupMessage} onClose={handleClosePopup} />)}
             {showPopupFail && (<Popup image={popupImageFail} message={popupMessageFail} onClose={handleClosePopupFail} />)}
         </div>
